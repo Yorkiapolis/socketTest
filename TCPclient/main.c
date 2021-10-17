f6d72686849a3fc76bc553276e18f8dff76b475d
yorkiapolis <zheyan.liu@qq.com>
2021-10-18 00:36:20 +0800

    completed version 1.0, multithread needed to be tested

--- TCPclient/main.c
+++ TCPclient/main.c
@@ -1,6 +1,65 @@
-#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <io_utils.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define ECHO_SERVICE_PORT 1035
+#define BUFSIZE 512
+
+typedef int SockDes;
 
 int main() {
-    printf("Hello, World!\n");
+    const char *arg_ip = "127.0.0.1";
+    SockDes _sock = socket(AF_INET, SOCK_STREAM,
+                          IPPROTO_IP);
+    if(_sock < 0){
+        perror("sock");
+        PRINTLNF("creating server-side socket failed");
+        exit(-1);
+    }
+
+    struct sockaddr_in servAddr = {
+            .sin_family = AF_INET,
+            .sin_port = htons(ECHO_SERVICE_PORT),
+            .sin_addr = inet_addr(arg_ip),
+    };
+    memset(&servAddr.sin_zero, 0,
+           sizeof(servAddr.sin_zero));
+    int connectRes = connect(_sock, (struct sockaddr *)&servAddr,
+            sizeof(servAddr));
+    if(connectRes < 0){
+        perror("connect");
+        exit(-1);
+    }
+
+    while(1){
+        char buf[BUFSIZE];
+        char *next = fgets(buf, BUFSIZE, stdin);
+        if(!next){
+            if(ferror(stdin)){
+                perror("client input error");
+                break;
+            }else if(feof(stdin)){
+                continue;
+            } else{
+                perror("unknown error");
+                break;
+            }
+        }
+        if(strcmp(next, "end") == 0){
+            close(_sock);
+            break;
+        }
+        write(_sock, buf, strlen(buf)+1);
+        memset(buf, 0, sizeof(buf));
+        ssize_t readRes = read(_sock, buf, BUFSIZE);
+        if(readRes < 0){
+            perror("receive");
+            exit(-1);
+        }
+        PRINTLNF("[ECHO] from server@[%s]: %s", arg_ip, buf);
+    }
+
     return 0;
 }
--- TCPserver/tcp_server.c
+++ TCPserver/tcp_server.c
@@ -8,29 +8,32 @@
 
 #define QUEUEBUFFER 16
 #define RECVBUF 512
+#define ECHO_SERVICE_PORT 1035
+#define DEBUG 0
 
 typedef int SockDes;
 typedef struct {
     struct sockaddr_in cliaddr;
     SockDes _sock;
-    void (*callback)(const char *);
+
+    void (*callback)(const char *, SockDes);
 } ComCliaddr;
 
-
-int Process_T(const ComCliaddr *cliaddrArg){
+int Process_T(const ComCliaddr *cliaddrArg) {
     char buf[RECVBUF];
     char *cliaddr = inet_ntoa(cliaddrArg->cliaddr.sin_addr);
-    while (1){
-        ssize_t _recvLen = read(cliaddrArg->_sock, buf,RECVBUF-1);
-        if(_recvLen > 0){
+    while (1) {
+        ssize_t _recvLen = read(cliaddrArg->_sock, buf, RECVBUF - 1);
+        if (_recvLen > 0) {
             buf[_recvLen] = '\0';
             PRINTLNF("receive from client@[%s]: %s", cliaddr, buf);
-            thrd_sleep(&(struct timespec){.tv_sec=2,}, NULL);
+            fflush(stdout);
+            thrd_sleep(&(struct timespec) {.tv_sec=2,}, NULL);
             write(cliaddrArg->_sock, buf, strlen(buf));
-        }else if(_recvLen == EOF){
-            cliaddrArg->callback(cliaddr);
+        } else if (_recvLen == 0) {
+            cliaddrArg->callback(cliaddr, cliaddrArg->_sock);
             break;
-        } else{
+        } else {
             PRINTLNF("\rread error[%ld]", _recvLen);
             break;
         }
@@ -38,50 +41,69 @@ int Process_T(const ComCliaddr *cliaddrArg){
     return 0;
 }
 
-void SockCloseCallback(const char *cliaddr){
+void SockCloseCallback(const char *cliaddr, SockDes _sktCLosed) {
+    close(_sktCLosed);
     PRINTLNF("\rCLient@[%s] is closed", cliaddr);
 }
 
 int main() {
     struct sockaddr_in servSockAddr = {
             .sin_family = AF_INET,
-            .sin_port = htons(1033),
-            .sin_addr = htonl(INADDR_ANY),
+            .sin_port = htons(ECHO_SERVICE_PORT),
+            .sin_addr = htonl(INADDR_LOOPBACK),
     };
     memset(&servSockAddr.sin_zero, 0,
            sizeof(servSockAddr.sin_zero));
 
     SockDes _sock = socket(AF_INET,
-                          SOCK_STREAM, IPPROTO_IP);
-    int bindRes = bind(_sock, (struct sockaddr *)&servSockAddr,
-            sizeof(servSockAddr));
-    if (bindRes != 0){
-        perror("");
+                           SOCK_STREAM, IPPROTO_IP);
+    if (_sock < 0) {
+        perror("sock");
+        PRINTLNF("creating server-side socket failed");
+        exit(-1);
+    }
+
+    int bindRes = bind(_sock, (struct sockaddr *) &servSockAddr,
+                       sizeof(servSockAddr));
+    if (bindRes != 0) {
+        perror("bind");
         PRINTLNF("\rbind failed, error[%d] occurs", bindRes);
         exit(-1);
     }
 
-    listen(_sock, QUEUEBUFFER);  // what if exceed the queue buffer size??
+    int listenRes = listen(_sock, QUEUEBUFFER);  // what if exceed the queue buffer size??
+    if (listenRes < 0) {
+        perror("listen");
+        PRINTLNF("\rlisten failed, error[%d] occurs", listenRes);
+        exit(-1);
+    }
 
-    for(;;){
+    PRINTLNF("Server@[%s] is waiting for connections...",
+             inet_ntoa(servSockAddr.sin_addr));
+    for (;;) {
         struct sockaddr_in cliaddr;
         ComCliaddr cliaddrArg;
         unsigned int cliaddr_len = sizeof(cliaddr);
-        PRINTLNF("Server@[%s] is waiting for connections...",
-                 inet_ntoa(servSockAddr.sin_addr));
-        SockDes _sockCLient = accept(_sock, (struct sockaddr *)&cliaddr,
-                &cliaddr_len);
-        if(_sockCLient == -1){
-            PRINTLNF("the acception from [%s] is failed",
+        SockDes _sockClient = accept(_sock, (struct sockaddr *) &cliaddr,
+                                     &cliaddr_len);
+        if (_sockClient == -1) {
+            PRINTLNF("the acceptation from [%s] is failed",
                      inet_ntoa(cliaddr.sin_addr));
             continue;
         }
+        PRINTLNF("connection with client@[%s] established",
+                 inet_ntoa(cliaddr.sin_addr));
         cliaddrArg.cliaddr = cliaddr;
-        cliaddrArg._sock = _sockCLient;
+        cliaddrArg._sock = _sockClient;
         cliaddrArg.callback = SockCloseCallback;
         thrd_t pro_t;
         thrd_create(&pro_t, (thrd_start_t) Process_T, &cliaddrArg);
         thrd_detach(pro_t);
+
+#if DEBUG
+        break;
+#endif
     }
+    close(_sock);
     return 0;
 }
